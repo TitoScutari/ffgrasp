7cea14dfec8b9c5a72d7c9f33820f729dbdeabf8
TitoScutari <titoscutari@LAPTOP-PGQSTQLO>
2023-04-08 01:40:10 +0200

    installation and dependencies

---
+++ install.sh
@@ -0,0 +1,6 @@
+  echo "setting up virtual environment" &&
+python -m venv . &&
+chmod 744 ./bin/activate &&
+./bin/activate &&
+  echo "installing dependencies" &&
+  pip install -r requirements.txt
